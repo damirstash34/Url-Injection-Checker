1cc295a4e2ded2f83d523fece818e8e6aba706af
damirstash34 <62292195+damirstash34@users.noreply.github.com>
2020-06-16 16:53:04 +0000

    Update modules_install_linux.sh

--- UIC/installs/modules_install_linux.sh
+++ UIC/installs/modules_install_linux.sh
@@ -1,6 +1,28 @@
-apt install colorama
-apt install time
-apt install argparse
-apt install os
-apt install sys
-apt install requests
+echo """
+----------------------
+|Choose your platform|
+|====================|
+| [1] Linux----------|
+| [2] Termux---------|
+|====================|
+|____________________|
+"""
+read platform
+
+if [ $platform  = '1' ]
+then
+	apt install colorama
+	apt install time
+	apt install argparse
+	apt install os
+	apt install sys
+	apt install requests
+elif [ $platform = '2' ]
+then
+	pkg install colorama
+	pkg install time
+	pkg install argparse
+	pkg install os
+	pkg install sys
+	pkg install requests
+fi
